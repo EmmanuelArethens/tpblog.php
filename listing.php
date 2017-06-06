cece4d52ccffbdbdf06a7e877300b9457c14ff7b
Manu Lanikeu <emmanuel.arethens1@gmail.com>
2017-06-06 10:33:23 +0200

    ajout de la modif de fichiers

--- listing.php
+++ listing.php
@@ -9,6 +9,14 @@
         $content = file_get_contents('textes/'.$file);
         echo '<p>'.$content.'</p>'.'<form action="delete.php" method="GET"><input type="submit" value="delete">
         <input type="hidden" name="paul" value="'.$file.'"></form>';
+        ?>
+        <form action="modify.php" method="post">
+        <input type="hidden" value="<?php echo basename($file, ".txt"); ?>" name="titi">
+        <input type="submit" value="modif">
+        </form>
+        <?php
     }
+
+
 
 ?>
---
+++ modify.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1> MN sup R FORMULèRE </h1>
+    <form action="create1.php" method="post">
+        <input type="text" value="<?php
+        $path = "textes/".$_POST['titi'].".txt";
+        if(isset($path)){
+            echo $_POST["titi"];
+        }
+        ?>" name="tit">
+        <textarea name="comment"><?php 
+        if(isset($path)){
+            echo file_get_contents($path);
+        }
+        ?></textarea>
+        <button>Submit</button>
+    </form>
+
+
+
+
+</body>
+
+</html>
